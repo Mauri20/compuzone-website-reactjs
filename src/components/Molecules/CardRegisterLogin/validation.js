96d53ca7d215e3aa477c988303e546964e472036
Andres Cordova <andrescordovaoficial@gmail.com>
2022-11-08 21:33:17 -0600

    Little changes on validations

--- src/components/Molecules/CardRegisterLogin/validation.js
+++ src/components/Molecules/CardRegisterLogin/validation.js
@@ -3,22 +3,35 @@ export const validate = (values) => {
   //validating the username is text and is required
   if (!values.userName) {
     errors.userName = 'Obligatorio';
-  } else if (!/^[A-Za-z ]+$/.test(values.userName)) {
-    errors.userName = 'El nombre de usuario solo debe contener letras';
+  } else if (!/^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/g.test(values.userName)) {
+    errors.userName = 'El nombre de usuario solo debe contener letras y comenzar con mayúscula';
+  } else if (values.userName.length < 10) {
+    errors.userName = 'El nombre de usuario no debe ser tan corto';
   }
   //validating the phone number is required and is a number with area from Central America
   if (!values.phone) {
     errors.phone = 'Obligatorio';
-  } else if (!/^[6-7]\d{3}-\d{4}$/.test(values.phone)) {
-    errors.phone = 'El número de teléfono debe tener el formato 6000-0000 o 7000-0000';
+  } else if( values.phone.length < 9 ) {
+    errors.phone = 'El número de teléfono debe contener al menos 8 caracteres';
   }
+  //==============================================
+  //if (!values.phone) {
+  //  errors.phone = 'Obligatorio';
+  //} else if (!/^[6-7]\d{3}-\d{4}$/.test(values.phone)) {
+  //  errors.phone = 'El número de teléfono debe tener el formato 6000-0000 o 7000-0000';
+  //}
+  //==============================================
   //validating the user is required
   if (!values.user) {
     errors.user = 'Obligatorio';
+  }else if(values.user.length < 5){
+    errors.user = 'El usuario debe contener al menos 5 caracteres';
   }
   //validating the address is required
   if (!values.address) {
     errors.address = 'Obligatorio';
+  }else if( values.address.length < 10 ){
+    errors.address = 'La dirección no debe ser tan corta';
   }
   //validating the password larger than 6 characters
   if (!values.password) {
